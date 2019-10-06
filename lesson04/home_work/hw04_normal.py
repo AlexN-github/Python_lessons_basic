319ff9e97875147446c04e9f8782a89489f655bb
Alex <n_alex2001@mail.ru>
2019-10-06 16:27:28 +0300

    Сделал все

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -21,6 +21,34 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
 
+print("Задание-1")
+print("Решение с помощью re")
+import re
+print(re.findall("[A-Z]*([a-z]+)[A-Z]*", line))
+
+print("Решение без re")
+ResList = []
+SubStr = ""
+Flag = False
+for item in line:
+       if item.islower():
+              if Flag:
+                     ResList.append(SubStr)
+                     SubStr = ""
+                     Flag = False
+              SubStr += item
+              continue
+       elif item.isupper():
+              Flag = True
+              continue
+       else:
+              SubStr = ""
+ResList.append(SubStr)
+print(ResList)
+
+
+
+
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
 # два символа в нижнем регистре, а справа - два символа в верхнем регистре.
@@ -45,9 +73,62 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+
+print("\nЗадание-2")
+import re
+print(re.findall("[a-z]{2}([A-Z]+)[A-Z]{2}", line_2))
+
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+print("\nЗадание-3")
+
+
+def InitFileData():
+       'Функция инициирования файла с данными'
+       def WriteFile(Data, DIR, FileName_DataRandom):
+           with open(os.path.join(DIR, FileName_DataRandom), 'w', encoding='UTF-8') as f:
+               f.writelines(Data)
+
+       import random
+       import os
+       #print(10**2500)
+       InitData = [str(random.randint(0, 9)) for _ in range(2500)]
+       InitData = "".join(InitData)
+       WriteFile(InitData, DIR, FileName_DataRandom)
+       #print("".join(InitData))
+
+def LoadFileData():
+       'Функция загрузки данных из файла'
+       import os
+       Data = ""
+       with open(os.path.join(DIR, FileName_DataRandom), 'r', encoding='UTF-8') as f:
+              for line in f:  # считываем файл построчно
+                            Data += line
+       return Data
+
+
+import re
+DIR = "data"
+FileName_DataRandom = "DataRandom.txt"
+InitFileData()
+Data = LoadFileData()
+print(Data)
+#Находим все последовательности из повторяющихся символов
+List = re.findall("[0]{2,}|[1]{2,}|[2]{2,}|[3]{2,}|[4]{2,}|[5]{2,}|[6]{2,}|[7]{2,}|[8]{2,}|[9]{2,}",Data)
+#Считаем количество символов в кадой последовательности
+CountList = [len(Item) for Item in List]
+#Находим максимальное значение повторений
+IndexMaxLen = CountList.index(max(CountList))
+print("Самая длинная последовательность:",List[IndexMaxLen])
+
+
+
+
+
+
